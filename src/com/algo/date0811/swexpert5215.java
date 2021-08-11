59863114e0b67225605ac3199c805e375ce595a9
eunyeong1113 <eylee1113@naver.com>
2021-08-11 15:19:15 +0900

    0811

---
+++ src/com/algo/date0811/swexpert5215.java
@@ -0,0 +1,58 @@
+package com.algo.date0811;
+
+import java.util.Scanner;
+
+public class swexpert5215 {
+	static int T, food, kal, sumKal, sumFood, MAX=0;
+	static int[][] menu;
+	static boolean[] isSelected;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			food = sc.nextInt();
+			kal = sc.nextInt();
+			isSelected = new boolean[food];
+			menu = new int[food][2];
+			for (int i = 0; i < food; i++) {
+				for (int j = 0; j < 2; j++) {
+					menu[i][j] = sc.nextInt();
+				}
+			} // menu[i][0]=점수,menu[i][1]=칼로리
+
+			subset(0);
+			System.out.println("#"+tc+" "+MAX);
+			MAX=0;
+		}
+
+	}
+
+	private static void subset(int cnt) {
+		
+		if (cnt == food) {
+			sumKal = 0;
+			sumFood = 0;
+			for (int i = 0; i < food; i++) {
+				if (isSelected[i]) {
+					sumKal += menu[i][1];
+					sumFood += menu[i][0];
+				}
+			}
+			if (sumKal <= kal) {
+				//System.out.println(sumKal+" "+sumFood);
+				if (MAX < sumFood) {
+					MAX=sumFood;
+				}
+			}
+			return;
+		}
+		//부분집합 코드
+		isSelected[cnt] = true;
+		subset(cnt + 1);
+		isSelected[cnt] = false;
+		subset(cnt + 1);
+
+	}
+
+}
